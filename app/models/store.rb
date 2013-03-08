b160e6234131cb4d7dcb1006293b3096490bf5af
jmorrison <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+L3hHIGDioJbV6AtUw5x0GimW7uuRJHBXIXpitZXFwoCiRvkXjIl0xT/ZawWC4tthCGlOHD36MCr3uU0Nhjln8oGKquegu8fsYziT6wa8T6NqkQbpi7LEUa2ETmcuyKzOfY2W394/Ul/N2J8OvUrTee5pLl6NvFJqTwoXI3LNUXVPiuAZV4m5l9VJs7SJsvCOrAVDPopunsUvgtNTD2+jIbM0RKFaZe+mPJEIbcjeqXJy4k1J+TXS+mM9jWj5FlcmuSRHBPAj7pLLM9fnWpbdRm5S8tHRkc6LjGfa98bd/pJGDPOMOlGVaMCZnoQLSeVxhagJTP88WD7c/lyXZbKV bluesunjcm@gmail.combluesunjcm@gmail.com>
2013-03-07 21:39:56 -0800

    Store.rb Validations

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,10 @@
 class Store < ActiveRecord::Base
   belongs_to :group
   attr_accessible :approved, :description, :name
+
+  #Validations by JM
+  validates :group, :presence => true
+  validates :approved, :presence => true
+  validates :description, :presence => true
+  validates :name, :presence => true
 end
