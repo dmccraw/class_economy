69f2cab97287b12d596bf52edc4e7725e38a5b0d
Dustin McCraw <dustin@lightspeedsystems.com>
2013-04-24 15:31:39 -0700

    annotations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,18 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :integer          not null, primary key
+#  owner_id   :integer
+#  owner_type :string(255)
+#  group_id   :integer
+#  balance    :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Account < ActiveRecord::Base
-  belongs_to :owner
+  belongs_to :owner, polymorphic: true
   belongs_to :group
   attr_accessible :balance
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :integer          not null, primary key
+#  owner_id   :integer
+#  owner_type :string(255)
+#  group_id   :integer
+#  balance    :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Account do
